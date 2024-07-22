78958c5997665667667fa9a00fb4fd5a7d493a96
evanbacon <bacon@expo.io>
2024-07-21 19:07:51 -0500

    fix web image

--- packages/mdx/src/getDOMComponents.tsx
+++ packages/mdx/src/getDOMComponents.tsx
@@ -1,4 +1,6 @@
+import registry from "@react-native/assets-registry/registry";
 import React, { Children } from "react";
+import { PixelRatio } from "react-native";
 
 export const OrderContext = React.createContext<{
   index: number;
@@ -7,6 +9,68 @@ export const OrderContext = React.createContext<{
   firstOfType: boolean;
   prevSibling: string;
 } | null>(null);
+
+function MDImage({ src, ...props }) {
+  const resolvedSrc = React.useMemo(() => {
+    if (typeof src === "number") {
+      return resolveAssetUri(registry.getAssetByID(src));
+    }
+    return src;
+  }, [src]);
+
+  return <img {...props} src={resolvedSrc} />;
+}
+
+const svgDataUriPattern = /^(data:image\/svg\+xml;utf8,)(.*)/;
+
+function resolveAssetUri(source) {
+  let uri: string | null = null;
+  if (typeof source === "number") {
+    // get the URI from the packager
+    let asset = registry.getAssetByID(source);
+    if (asset == null) {
+      throw new Error(
+        'Image: asset with ID "' +
+          source +
+          '" could not be found. Please check the image source or packager.'
+      );
+    }
+    let scale = asset.scales[0];
+    if (asset.scales.length > 1) {
+      const preferredScale = PixelRatio.get();
+      // Get the scale which is closest to the preferred scale
+      scale = asset.scales.reduce((prev, curr) =>
+        Math.abs(curr - preferredScale) < Math.abs(prev - preferredScale)
+          ? curr
+          : prev
+      );
+    }
+    const scaleSuffix = scale !== 1 ? "@" + scale + "x" : "";
+    uri = asset
+      ? asset.httpServerLocation +
+        "/" +
+        asset.name +
+        scaleSuffix +
+        "." +
+        asset.type
+      : "";
+  } else if (typeof source === "string") {
+    uri = source;
+  } else if (source && typeof source.uri === "string") {
+    uri = source.uri;
+  }
+  if (uri) {
+    const match = uri.match(svgDataUriPattern);
+    // inline SVG markup may contain characters (e.g., #, ") that need to be escaped
+    if (match) {
+      const prefix = match[1];
+      const svg = match[2];
+      const encodedSvg = encodeURIComponent(svg);
+      return "" + prefix + encodedSvg;
+    }
+  }
+  return uri;
+}
 
 /**
  * Get base elements that work natively to the browser. If the native app has any of these views built-in, then they can be used there too.
@@ -36,7 +100,7 @@ export function getDOMComponents() {
     "br",
     "mark",
     "code",
-    "inlineCode",
+
     "pre",
     "time",
     "strong",
@@ -70,6 +134,8 @@ export function getDOMComponents() {
     ...Object.fromEntries(passthroughElements),
 
     Wrapper: RootWrapper,
+    inlineCode: stripExtras("code"),
+    img: stripExtras(MDImage, "img"),
   };
 }
 
